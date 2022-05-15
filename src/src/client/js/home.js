976514308d95232c4282819fe355139f6354acb2
koo <gudonghee2000@gmail.com>
2022-05-16 00:04:22 +0900

    f

--- src/src/client/js/home.js
+++ src/src/client/js/home.js
@@ -1,7 +1,14 @@
-var audio = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/Lecrae_-_Anomaly_(Lyric_Video).mp3');
-audio.volume = 0.5;
-audio.autoplay = true;
-audio.play();
+var soundHandle = new Audio();
+
+$(document).ready(function() {
+    addEventListener('touchstart', function (e) {
+        soundHandle.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/Lecrae_-_Anomaly_(Lyric_Video).mp3';
+        soundHandle.loop = true;
+        audio.volume = 0.5;
+        soundHandle.play();
+    });
+});
+
 
 var typingBool = false;
 var buttonBool = false;
