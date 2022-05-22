a1152bbd89608cd6c264eec734589bed6c945d33
Jeongseunghun <seunghun311@naver.com>
2022-05-22 10:46:17 +0900

    f

--- src/lastMessage.py
+++ src/lastMessage.py
@@ -6,7 +6,7 @@ Pororo.available_tasks()
 zsl = Pororo(task="zero-topic", lang="ko")
 
 def getMessage(message): 
-    print(zsl(message, ['건강', '연애', '인간관계', '자아', '취업', '학업', '금전']))
+    print(zsl(message, ['건강', '연애', '인간관계', '자아', '취업', '학업', '금전', '직장']))
     
 if __name__ == '__main__' : 
     getMessage(sys.argv[1])
